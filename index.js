f4cfffa681d2165c83d2c12f361b9fbdebc2878b
Alex Sanchez <lockedasanchez@gmail.com>
2024-03-18 13:13:35 -0400

    complete lab (3/18)

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+const { objectContaining } = require("expect");
+
 // Code your solution here
+function findMatching(Drivers, string){
+    const lowercaseString = string.toLowerCase();
+    let list = Drivers.filter(driver => {
+        const lowerCaseDriver = driver.toLowerCase();
+        return lowerCaseDriver.includes(lowercaseString);
+    })
+return list;
+}
+function fuzzyMatch(drivers, query){
+
+    let list = drivers.filter(driver =>  driver.startsWith(query))
+    return list;
+}
+function matchName(drivers, string) {
+    return drivers.filter(function(driver) {
+      return driver.name === string;
+    });
+  }
